e8e78ee37cc5bdc51af5036b7ab1d66a9efb969b
0m1n3m <ominem313@gmail.com>
2026-04-20 15:45:23 +0100

    Add AdminUserSeeder to create verified admin user

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminUserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        User::updateOrCreate(
+            ['email' => 'omar.gonzalez.consultaeuropa@gmail.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('0Mar*09!01!21'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ]
+        );
     }
 }
