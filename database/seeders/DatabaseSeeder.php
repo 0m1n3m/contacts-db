d17a6cad68bd68a9466820d2a3f10c386014836c
Omar Gonzalez <ominem313@gmail.com>
2026-04-29 11:37:07 +0100

    updated database seeder to call only admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             AdminUserSeeder::class,
-            EditorUserSeeder::class,
-            ViewerUserSeeder::class,
         ]);
     }
 }
